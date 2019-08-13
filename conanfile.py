9c4b9d6daa89a6fc801e96f0e49645049e24f103
B. van Lew <b.van_lew@lumc.nl>
2019-08-13 08:49:02 +0200

    Build only shared true

--- conanfile.py
+++ conanfile.py
@@ -56,7 +56,7 @@ class QtConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type", "os_build", "arch_build"
 
     options = dict({
-        "shared": [True, False],
+        "shared": [True],
         "commercial": [True, False],
 
         "opengl": ["no", "es2", "desktop", "dynamic"],
