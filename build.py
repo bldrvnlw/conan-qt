0ba35a68ac90980d92a9481d31efe630f362e836
B. van Lew <b.van_lew@lumc.nl>
2019-08-13 09:19:50 +0200

    Filter no share builds out in build.py

--- build.py
+++ build.py
@@ -6,4 +6,14 @@ from bincrafters import build_template_default
 if __name__ == "__main__":
 
     builder = build_template_default.get_builder(build_policy="missing")
+    
+    # allow only shared builds
+    filtered_builds = []
+    for settings, options, env_vars, build_requires, reference in builder.items:
+        # Build shared only
+        if not options["Qt:shared"]:
+            continue
+            
+        filtered_builds.append([settings, new_options, env_vars, build_requires])        
+    builder.builds = filtered_builds
     builder.run()
